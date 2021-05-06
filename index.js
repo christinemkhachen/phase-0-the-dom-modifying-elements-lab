5348347516af1746fd09c982e1bf5bee6c200c2d
christinemkhachen <christinemkhachen@gmail.com>
2021-05-06 16:16:49 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+var main = document.getElementById("main")
+    main.remove()
+    
+const newHeader = document.createElement("h1")
+    newHeader.setAttribute("id","victory")
+        document.body.appendChild(newHeader)
+newHeader.innerHTML = "Christine is the champion"
